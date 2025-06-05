012c7aea270c4f047bfaaf90ad90bbd94e244c6b
Mando-Codes <ciaranmonteith3@gmail.com>
2025-06-06 09:31:14 +1200

    commit

--- Practice Files/car-physics-tutorial/Game Elements/Scripts/mainmenu.gd
+++ Practice Files/car-physics-tutorial/Game Elements/Scripts/mainmenu.gd
@@ -1,11 +1,14 @@
 extends Control
-
+@onready var sprint_information: Label = $"Sprint Information Text"
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Game Elements/Scenes/main.tscn")
 
 func _on_information_pressed() -> void:
-	pass # Replace with function body.
+	sprint_information.show()
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+func _on_close_information_pressed() -> void:
+	sprint_information.hide()
