30f26124f1fd9f1022b5ff79ecbc88585d4fa772
Kris Zhang <kriszhang122507@gmail.com>
2025-07-15 11:44:30 +1200

    Uploaded game folder,

---
+++ delivaura/Game Elements/Scripts/check_point.gd
@@ -0,0 +1,27 @@
+extends Area3D
+
+@onready var aura_point_menu = $"../Aura Point Menu"
+@onready var aura_point_timer_bar = $"../Player/Aura Point Timer Bar"
+@onready var aura_point_counter = $"../Player/Aura Point Counter"
+
+func _ready() -> void:
+	add_to_group("CheckPoints")
+
+func _on_body_entered(_body: Node3D) -> void:
+	var isPlayer = get_tree().get_first_node_in_group("Player")
+	if isPlayer:
+		if Global.timer_is_running == false: 
+			print("The Player Has Entered.")
+			Global.PauseMenu(aura_point_menu)
+		else:
+			Global.timer_is_running = false
+			Global.aura_point_timer.stop()
+			aura_point_timer_bar.hide()
+			Global.Initialise_Timer_Bar(aura_point_timer_bar)
+			Global.total_aura_points += 10
+
+func _on_body_exited(_body: Node3D) -> void:
+	var isPlayer = get_tree().get_first_node_in_group("Player")
+	if isPlayer: 
+		print("The Player Has Exited.")
+		global_position = Vector3(92.261, 2.195, 63.176)
