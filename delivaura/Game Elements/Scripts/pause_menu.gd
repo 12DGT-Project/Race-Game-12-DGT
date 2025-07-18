9dd2a941cb0152e8be9b4243f427669d2efcad6c
Mando-Codes <ciaranmonteith3@gmail.com>
2025-07-18 14:38:23 +1200

    commit

--- delivaura/Game Elements/Scripts/pause_menu.gd
+++ delivaura/Game Elements/Scripts/pause_menu.gd
@@ -1,12 +1,16 @@
 extends CanvasLayer
 @onready var pause_menu = $"."
+@onready var controls: Label = $Controls
 
 
 func _on_resume_pressed():
 	Global.PauseMenu(pause_menu)
 
 func _on_display_controls_pressed():
-	pass # Replace with function body.
+	controls.show()
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+func _on_close_controls_pressed() -> void:
+	controls.hide()
