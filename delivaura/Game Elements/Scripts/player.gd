3fad919e257fb20a1a0d7e18d4e1fb2cfbe22f76
Mando-Codes <ciaranmonteith3@gmail.com>
2025-07-23 13:09:26 +1200

    com

--- delivaura/Game Elements/Scripts/player.gd
+++ delivaura/Game Elements/Scripts/player.gd
@@ -4,15 +4,19 @@ extends Node3D
 @onready var aura_point_counter = $"Aura Point Counter"
 @onready var player_coords: Label = $"Player Coordinates"
 @onready var city: Node3D = $".."
+@onready var player: Node3D = $"."
 
 
 var base_label = "Total Aura Points: %s"
+var last_coord = Vector3.ZERO
 
 func _ready():
 	Global.Initialise_Timer_Bar(aura_point_timer_bar)
 
 func _process(_delta):
-	player_coords.text = str(Global.getglobal_position)
+	if last_coord != player.global_position:
+		last_coord = player.global_position
+		player_coords.text = str(player.global_position)
 	aura_point_timer_bar.value = Global.aura_point_timer.time_left
 	aura_point_counter.text = base_label % Global.total_aura_points
 	if Global.timer_is_running == false:
